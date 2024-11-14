c3a8c525b3c715c15761fa88542240dfe13ceaa5
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2024-11-14 15:27:56 +0100

    🌴

--- python_packages/directus_graphql_helpers/directus_graphql_helpers/__init__.py
+++ python_packages/directus_graphql_helpers/directus_graphql_helpers/__init__.py
@@ -2,17 +2,17 @@ import json
 import requests
 
 
-def get_access_token(secret):
+def get_access_token(email, password, url):
     query = f"""mutation {{
-      auth_login(email: "{secret["email"]}", password: "{secret["password"]}") {{
+      auth_login(email: {email}, password: "{password}") {{
         access_token
         refresh_token
       }}
     }}"""
-    r = requests.post(secret["url"] + '/graphql/system', json={'query': query})
+    r = requests.post(url + '/graphql/system', json={'query': query})
     return r.json()['data']['auth_login']['access_token']
 
 
-def graphql_query(query, secret):
-    r = requests.post(f"{secret['url']}/graphql/?access_token={get_access_token(secret)}", json={'query': query})
+def graphql_query(query, email, password, url):
+    r = requests.post(f"{url}/graphql/?access_token={get_access_token(email, password, url)}", json={'query': query})
     return json.loads(r.text)
